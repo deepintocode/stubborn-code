24a0bb4c36870458aa5a028c86ca138090c911ce
Teo <todorare@gmail.com>
2020-07-22 10:03:26 +0100

    add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,9 @@ module.exports = {
     require("tailwindcss"),
     require("postcss-nesting"),
     require("autoprefixer"),
-    process.env.NODE_ENV === "production" &&
-      require("@fullhuman/postcss-purgecss")({
-        content: ["./_site/**/*.html"],
-        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      }),
+    require("@fullhuman/postcss-purgecss")({
+      content: ["./_site/**/*.html"],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
   ],
 };
